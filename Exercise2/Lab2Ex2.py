978fd3838a76d4e6daeef981079ef9662c40e7bf
javierz420 <javierzsj30.22@ichat.sp.edu.sg>
2023-10-23 10:53:26 +0800

    Ex2 v1.0

---
+++ Exercise2/Lab2Ex2.py
@@ -0,0 +1,8 @@
+def display_main_menu():
+    print("Enter some numbers separated by commas (e.g. 5, 67, 32")
+
+def get_user_input():
+    input()
+    x = str.split(", ")
+    numlist = list((x))
+    print(numlist)
