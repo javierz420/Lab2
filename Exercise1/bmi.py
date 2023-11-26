984e6c6b1df26657cdeeebd8a5e813d15034718a
javierz420 <javierzsj30.22@ichat.sp.edu.sg>
2023-11-27 00:10:12 +0800

    Lab3 BMI

--- Exercise1/bmi.py
+++ Exercise1/bmi.py
@@ -1,15 +1,17 @@
 def calculate_bmi(height, weight):
     print("Height = " + str(height))
     print("Weight = " + str(weight))
-    bmi = weight / (height * height)
-    print(bmi)
-
+    bmi = weight/(height * height)
+    print("BMI = ", round(bmi, 2))
     if bmi < 18.5:
-        print("b is greater than a")
-    elif 18.5 <= bmi <= 25.0:
-        print("Normal Weight")
+        print("Under Weight")
+        return -1
+    elif bmi > 25.0:
+        print("Over Weight")
+        return 1
     else:
-        print("Overweight")
+        print("Normal Weight")
+        return 0
 
 
-calculate_bmi(weight=57, height=1.73)
+print(calculate_bmi(weight=57, height=1.73))
